7ce6a506305b8a53811796babe76c94dee3b56c0
root <root@LAPTOP-FRJK5PVM>
2024-09-23 11:24:21 +0900

    commit

--- app/lib/prisma.tsx
+++ app/lib/prisma.tsx
@@ -1,14 +1,26 @@
 import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
+// let prisma: PrismaClient;
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined;
-};
+// const globalForPrisma = global as unknown as {
+//   prisma: PrismaClient | undefined;
+// };
 
-if (!globalForPrisma.prisma) {
-  globalForPrisma.prisma = new PrismaClient();
-}
-prisma = globalForPrisma.prisma;
+// if (!globalForPrisma.prisma) {
+//   globalForPrisma.prisma = new PrismaClient();
+// }
+// prisma = globalForPrisma.prisma;
+const prisma: PrismaClient = (() => {
+  const globalForPrisma = global as unknown as {
+    prisma: PrismaClient | undefined;
+  };
+
+  if (!globalForPrisma.prisma) {
+    globalForPrisma.prisma = new PrismaClient();
+  }
+
+  return globalForPrisma.prisma;
+})();
+
 
 export default prisma;
